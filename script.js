de1824069e7fd5fb4370d4a593b3512d46602ea5
wolf <devcevu@gmail.com>
2024-06-25 12:36:43 +0200

    add turn logic

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const myModule = (function () {
         { name: "player1", piece: "O" },
         { name: "player2", piece: "X" },
     ];
-
+    let playersTurn;
     function createGame() {
         function printBoard() {
             console.log(
@@ -16,30 +16,46 @@ const myModule = (function () {
             );
         }
 
-        function playMove(playerIndex, row, col) {
-            let player = players[playerIndex];
-            let playerPiece = player && player.piece ? player.piece : null;
-
-            if (!playerPiece) {
-                console.log("Player doesn't exist");
+        function playPlayer1(row, col) {
+            let playerPiece = players[0].piece;
+            if (playersTurn === 2) {
+                console.log("It's not your turn");
                 return;
             }
+
             if (!(row < board.length && col >= 0 && col < board[row].length)) {
-                console.log("Wrong cordinates");
+                console.log("Wrong cordinates, play again");
             } else if (board[row][col] !== null) {
-                console.log("The space is occupied");
+                console.log("The space is occupied, play again");
             } else {
                 board[row][col] = playerPiece;
                 printBoard();
+                playersTurn = 2;
+            }
+        }
+        function playPlayer2(row, col) {
+            let playerPiece = players[1].piece;
+            if (playersTurn === 1) {
+                console.log("It's not your turn");
+                return;
+            }
+            if (!(row < board.length && col >= 0 && col < board[row].length)) {
+                console.log("Wrong cordinates, play again");
+            } else if (board[row][col] !== null) {
+                console.log("The space is occupied, play again");
+            } else {
+                board[row][col] = playerPiece;
+                printBoard();
+                playersTurn = 1;
             }
         }
 
         printBoard();
         return {
-            playMove,
+            playPlayer1,
+            playPlayer2,
         };
     }
-
     return {
         createGame: createGame,
     };
