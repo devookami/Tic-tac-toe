0d2e9ea10ea8ef0ac0c8fe123174f446e680a3f0
wolf <devcevu@gmail.com>
2024-06-25 11:19:29 +0200

    add play game function without rules

--- script.js
+++ script.js
@@ -4,14 +4,39 @@ const myModule = (function () {
         [1.0, 1.1, 1.2],
         [2.0, 2.1, 2.2],
     ];
+
     function createGame() {
         function printBoard() {
             console.log(
                 board.map((row) => row.join(" | ")).join("\n---------\n")
             );
         }
+        //
+        function play(row, col, value) {
+            if (typeof value !== "string") {
+                console.log("Niste uneli teks");
+                return;
+            }
 
+            value = value.toUpperCase();
+
+            if (value !== "O" && value !== "X") {
+                console.log("Niste uneli O ili X ");
+                console.log(`Uneli ste ${value}`);
+                return;
+            }
+
+            if (!(row < board.length && col >= 0 && col < board[row].length)) {
+                console.log("Uneli ste pogresne kordinate");
+            } else {
+                board[row][col] = value;
+                printBoard();
+            }
+        }
         printBoard();
+        return {
+            play,
+        };
     }
     return {
         createGame: createGame,
