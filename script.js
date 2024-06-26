dc02f66ef255539f2458e711eb83ceb00dba6722
wolf <devcevu@gmail.com>
2024-06-26 13:34:29 +0200

    fix little bug

--- script.js
+++ script.js
@@ -79,6 +79,7 @@ function checkWin() {
 }
 
 function checkDraw() {
+    let boardisFull = 0;
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board.length; j++) {
             if (!(board[i][j] == null)) {
